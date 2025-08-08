0818a86e5b4d69ec43ca9d964f3f87467bf8d69a
Демидов Роман <demidov@kinoplan.ru>
2025-08-08 11:44:19 +0300

    Обновление Task2

--- src/main/scala/Task2.scala
+++ src/main/scala/Task2.scala
@@ -4,14 +4,22 @@
 // - Целое число является палиндромом, когда оно читается назад также как и вперед.
 // - Постарайтесь решить задачу без преобразования числа в строку.
 
+import scala.annotation.tailrec
+
 object Task2 {
   def solution(x: Int): Boolean = {
+    @tailrec
+    def loop(n: Int, accumulator: Int): Int = {
+      if (n == 0)
+        accumulator/10
+      else
+        loop(n / 10, (accumulator + n % 10) * 10)
+    }
 
-    if (x < 0 || (x % 10 == 0 && x != 0)) return false
-
-    val digit = LazyList.iterate(x)(n => n / 10).takeWhile(_ > 0).map(n => n % 10).toList
-    digit == digit.reverse
-
+    if(x > 0)
+      loop(x, 0) == x
+    else
+      false
   }
 
   println(s"Task 2 = ${solution(121)}")
@@ -25,4 +33,7 @@ object Task2 {
 
   println(s"Task 2 = ${solution(-101)}")
   // Task 2 = false
+
+  println(s"Task 2 = ${solution(11)}")
+  // Task 2 = true
 }
